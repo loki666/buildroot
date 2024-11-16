2ee6c9d2441d15203ea73d6e2367f549adb1761e
Philippe Simons <simons.philippe@gmail.com>
2024-11-16 21:40:22 +0100

    kernel driver ok but no working userspace

--- package/mali-driver/mali-driver.mk
+++ package/mali-driver/mali-driver.mk
@@ -4,12 +4,25 @@
 #
 ################################################################################
 
-MALI_DRIVER_VERSION = d40c57d81c9cf4bc6fefeaf47153fde8eed19ef2
-MALI_DRIVER_SITE = $(call github,bootlin,mali-driver,$(MALI_DRIVER_VERSION))
+MALI_DRIVER_VERSION = r51p0-00eac0
+MALI_DRIVER_SOURCE = BX304L01B-SW-99002-${MALI_DRIVER_VERSION}.tar
+MALI_DRIVER_SITE = https://developer.arm.com/-/media/Files/downloads/mali-drivers/kernel/mali-bifrost-gpu
 MALI_DRIVER_DEPENDENCIES = linux
-MALI_DRIVER_LICENSE = GPL-2.0
-MALI_DRIVER_LICENSE_FILES = LICENSE
-MALI_DRIVER_MODULE_SUBDIRS = r8p0/drivers/gpu/arm/midgard
 
-$(eval $(kernel-module))
+define MALI_DRIVER_BUILD_CMDS
+	$(MAKE) ARCH=${KERNEL_ARCH} KDIR=${LINUX_DIR} -C $(@D)/product/kernel/drivers/gpu/arm/midgard \
+		$(TARGET_CONFIGURE_OPTS) \
+		CONFIG_MALI_MIDGARD=m \
+		CONFIG_MALI_PLATFORM_NAME=sunxi \
+		CONFIG_MALI_REAL_HW=y \
+		CONFIG_MALI_DEVFREQ=y \
+		CONFIG_MALI_GATOR_SUPPORT=n \
+		all
+endef
+
+define MALI_DRIVER_INSTALL_TARGET_CMDS
+	cp $(@D)/product/kernel/drivers/gpu/arm/midgard/mali_kbase.ko \
+		$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)
+endef
+
 $(eval $(generic-package))
